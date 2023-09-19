e9116a03d63cfab3a61ad253d2b6beabb4df9f88
seokjinwoo <34178703+seokjinwoo@users.noreply.github.com>
2023-09-19 14:12:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,10 +27,22 @@ df2 = df2.rename(columns={  # 수정된 부분
     'OUT_RT': 'pro'
 })
 
+
+url2 = 'https://raw.githubusercontent.com/seokjinwoo/streamlit-example/master/income_data.xlsx'
+df3 = load_data(url2)
+
+df3 = df3.rename(columns={
+    'OJ_YY': 'year',
+    'OJ_M': 'month',
+    'ISMOK_NM': 'cat',
+    'RV_AGGR_AMT': 'amount'
+})
+
+
 # 페이지 선택을 위한 사이드바 메뉴 생성
 page = st.sidebar.selectbox(
     "예산 혹은 국세 진도율 선택",
-    ["2024년 예산 현황", "2023년 국세 진도율"]
+    ["2024년 예산 현황", "2023년 국세 진도율", "2023년 국세 수입 금액(3D)"]
 )
 
 # 선택된 페이지에 따라 내용 표시
@@ -57,3 +70,40 @@ elif page == "2023년 국세 진도율":
     ax.set_ylabel('Revenue progress rate (%)')
     ax.legend()
     st.pyplot(fig)
+    
+elif page == "2023년 국세 수입 금액(3D)":    
+    st.title("국세 수입 추이")
+    selected_cat = st.selectbox("세목:", df3['cat'].unique())
+    filtered_data = df3[df3['cat'] == selected_cat]
+    
+    # year가 2023이 아닌 데이터에 대한 라인
+    fig = px.line_3d(filtered_data[filtered_data['year'] != 2023], 
+                 x='year', 
+                 y='month', 
+                 z='amount', 
+                 color='year')
+
+    # year가 2023인 데이터에 대한 라인 (눈에 띄게 표시)
+    fig.add_trace(go.Scatter3d(
+    x=filtered_data[filtered_data['year'] == 2023]['year'],
+    y=filtered_data[filtered_data['year'] == 2023]['month'],
+    z=filtered_data[filtered_data['year'] == 2023]['amount'],
+    mode='lines',
+    line=dict(color='red', width=5),
+    name='2023'
+    ))
+
+    # 그래프 제목 및 축 이름 설정 및 그래프 사이즈 조정
+    fig.update_layout(
+        title=f"3D Line Graph for {selected_cat}",
+        scene=dict(
+            xaxis_title='Year',
+            yaxis_title='Month',
+            zaxis_title='Amount'
+        ),
+        width=1000,  # 그래프 너비
+        height=600   # 그래프 높이
+    )
+
+    # Streamlit에서 그래프 표시
+    st.plotly_chart(fig)
